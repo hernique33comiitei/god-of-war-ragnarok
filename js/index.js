e3a1beabda2c315a6636db85100a6bc37f9c2bda
henrique33comiitei <mulekinb@gmail.com>
2022-11-27 19:59:47 -0500

    fix error animation

--- js/index.js
+++ js/index.js
@@ -20,49 +20,11 @@ ScrollReveal().reveal('.container__trailer', {
 
 // CONFIGS PARA O CONTAINER PERSONAGENS 
 
-const body = document.body
 
-window.addEventListener('resize', () => { // CONFIGS PARA TROCAR A ANIMATION CASO SEJA MOBILE
-    let windowWidth = window.screen.width
-    if (windowWidth >= 650) {
-        animationComponentsDesk()
-    } else {
-        animationComponentsMobile()
-    }
+ScrollReveal().reveal('.container__personagensDivScroll', {
+    duration: 1000,
+    easing: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
+    distance: '100%',
+    origin: 'bottom'
 })
 
-function animationComponentsDesk() {
-    const defaultPersonagens = {
-        duration: 1000,
-        easing: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
-        distance: '100%',
-        origin: 'top',
-    }
-    
-    ScrollReveal().reveal('.kratos', defaultPersonagens)
-    ScrollReveal().reveal('.thor', defaultPersonagens)
-    ScrollReveal().reveal('.atreus', {
-        duration: 1000,
-        easing: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
-        distance: '100%',
-        origin: 'bottom'
-    })
-}
-
-function animationComponentsMobile() {
-    ScrollReveal().reveal('.container__personagensDivScroll', {
-        duration: 1000,
-        easing: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
-        distance: '100%',
-        origin: 'bottom'
-    })
-}
-
-function loadTestAnimation() {
-    let windowWidth = window.screen.width
-    if (windowWidth >= 650) {
-        animationComponentsDesk()
-    } else {
-        animationComponentsMobile()
-    }
-}
